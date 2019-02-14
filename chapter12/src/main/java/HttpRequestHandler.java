8b5185b35f860cd9e2896a645884312f3a902b5a
BSulima <arkadiusz.kawka.extern@ista.com>
2019-02-14 15:13:23 +0100

    chapter 12 not completed

---
+++ chapter12/src/main/java/HttpRequestHandler.java
@@ -0,0 +1,88 @@
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.DefaultFileRegion;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpHeaders;
+import io.netty.handler.codec.http.HttpResponse;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.HttpVersion;
+import io.netty.handler.codec.http.LastHttpContent;
+import io.netty.handler.ssl.SslHandler;
+import io.netty.handler.stream.ChunkedNioFile;
+
+import java.io.File;
+import java.io.RandomAccessFile;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+public class HttpRequestHandler extends SimpleChannelInboundHandler<FullHttpRequest> {
+    private final String wsUri;
+    private static File INDEX;
+
+    static {
+        URL location = HttpRequestHandler.class
+                .getProtectionDomain()
+                .getCodeSource()
+                .getLocation();
+
+        try {
+            String path = location.toURI() + "index.html";
+            path = !path.contains("file:") ? path : path.substring(5);
+            INDEX = new File(path);
+        } catch (URISyntaxException ex) {
+            throw new IllegalStateException(
+                    "Unable to locate index.html", ex);
+        }
+    }
+
+    public HttpRequestHandler(String wsUri) {
+        this.wsUri = wsUri;
+    }
+
+    protected void channelRead0(ChannelHandlerContext ctx, FullHttpRequest fullHttpRequest) throws Exception {
+        if (wsUri.equalsIgnoreCase(fullHttpRequest.getUri())) {
+            ctx.fireChannelRead(fullHttpRequest.retain());
+        } else {
+            if (HttpHeaders.is100ContinueExpected(fullHttpRequest)) {
+                send100Continue(ctx);
+            }
+            RandomAccessFile file = new RandomAccessFile(INDEX, "r");
+            HttpResponse response = new DefaultFullHttpResponse(
+                    fullHttpRequest.getProtocolVersion(), HttpResponseStatus.OK);
+            response.headers().set(
+                    HttpHeaders.Names.CONTENT_TYPE, "text/plain; charset=UTF-8");
+            boolean keepAlive = HttpHeaders.isKeepAlive(fullHttpRequest);
+            if (keepAlive) {
+                response.headers().set(
+                        HttpHeaders.Names.CONTENT_LENGTH, file.length());
+                response.headers().set(HttpHeaders.Names.CONNECTION, HttpHeaders.Values.KEEP_ALIVE);
+            }
+            ctx.write(response);
+            if (ctx.pipeline().get(SslHandler.class) == null) {
+                ctx.write(new DefaultFileRegion(file.getChannel(), 0, file.length()));
+            } else {
+                ctx.write(new ChunkedNioFile(file.getChannel()));
+            }
+            ChannelFuture cf = ctx.writeAndFlush(LastHttpContent.EMPTY_LAST_CONTENT);
+            if (!keepAlive) {
+                cf.addListener(ChannelFutureListener.CLOSE);
+            }
+        }
+    }
+
+    private static void send100Continue(ChannelHandlerContext ctx) {
+        FullHttpResponse response = new DefaultFullHttpResponse(
+                HttpVersion.HTTP_1_1, HttpResponseStatus.CONTINUE);
+        ctx.writeAndFlush(response);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
